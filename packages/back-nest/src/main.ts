a42b8ca231ab74f3102d56d16d5cb63e549ec3b1
Oleksandr Agniev <70901180+agniev-a-hub@users.noreply.github.com>
2024-08-18 22:43:10 +0200

    202408182240-adjusting-setup-vite-fe-libs (#2)

--- packages/back-nest/src/main.ts
+++ packages/back-nest/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvService } from './env/env.service';
 
-async function bootstrap() {
+async function start() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  const configService = app.get(EnvService);
+  const port = configService.get('BACK_NEST_APP_PORT');
+  await app.listen(port);
+};
+
+start();
