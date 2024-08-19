df0e39f9b3882bf9513527dbc09b073abfbca6ad
Oleksandr Agniev <70901180+agniev-a-hub@users.noreply.github.com>
2024-08-19 12:32:32 +0200

    202408191231-lint-prettier-setup-and-changes-made-etc (#4)

--- packages/front-react/src/routes/__root.tsx
+++ packages/front-react/src/routes/__root.tsx
@@ -1,10 +1,10 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2 text-red-400">
+      <div className="flex gap-2 p-2 text-red-400">
         <Link to="/" className="hover:text-cyan-600">
           Home
         </Link>{' '}
--- packages/front-react/vite.config.ts
+++ packages/front-react/vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import EnvironmentPlugin from 'vite-plugin-environment';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig(({ mode }) => {
   /* ENV management */
   const env = loadEnv(mode, process.cwd(), '');
-  const {
-    NODE_ENV,
-    FRONT_REACT_APP_PORT,
-    FRONT_REACT_CONTAINER_PORT,
-  } = env;
+  const { NODE_ENV, FRONT_REACT_APP_PORT, FRONT_REACT_CONTAINER_PORT } = env;
   const loadedEnv = {
     NODE_ENV,
     FRONT_REACT_APP_PORT,
@@ -18,11 +14,7 @@ export default defineConfig(({ mode }) => {
   };
 
   return {
-    plugins: [
-      react(),
-      EnvironmentPlugin(loadedEnv),
-      TanStackRouterVite(),
-    ],
+    plugins: [react(), EnvironmentPlugin(loadedEnv), TanStackRouterVite()],
     server: {
       port: Number(FRONT_REACT_APP_PORT),
       host: true,
