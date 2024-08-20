e4d7d605d79c81640c2b89639488668670916f3f
Oleksandr Agniev <70901180+agniev-a-hub@users.noreply.github.com>
2024-08-20 22:26:20 +0200

    202408202225-from-tailwind-to-mui-switch (#5)

--- packages/front-react/vite.config.ts
+++ packages/front-react/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
 
 export default defineConfig(({ mode }) => {
   /* ENV management */
@@ -15,6 +16,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react(), EnvironmentPlugin(loadedEnv), TanStackRouterVite()],
+    tools: {
+      rspack: {
+        plugins: [TanStackRouterRspack()],
+      },
+    },
     server: {
       port: Number(FRONT_REACT_APP_PORT),
       host: true,
